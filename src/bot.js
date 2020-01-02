0f45976840df820c971d33990e7e0cf88e44cf66
Itai Fish <chessandpotatoes@gmail.com>
2020-01-02 14:10:09 -0500

    Fixed weird error with tabs vs spaces

--- src/bot.js
+++ src/bot.js
@@ -4,66 +4,66 @@ const fs = require('fs');
 
 class Bot {
 
-    constructor(prefix, delimiter) {
-        this.id = Math.random().toString(36).substr(2, 9);
-        this.prefix = prefix;
-        this.delimiter = delimiter;
-        this.commands = new Discord.Collection();
-        const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-        for(const file of commandFiles) {
-            const command = require(`./commands/${file}`);
-            for(const commandObj of command.commands) {
-                this.commands.set(commandObj.name, commandObj);
-            }
-        }
-    }
+	constructor(prefix, delimiter) {
+		this.id = Math.random().toString(36).substr(2, 9);
+		this.prefix = prefix;
+		this.delimiter = delimiter;
+		this.commands = new Discord.Collection();
+		const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+		for(const file of commandFiles) {
+			const command = require(`./commands/${file}`);
+			for(const commandObj of command.commands) {
+				this.commands.set(commandObj.name, commandObj);
+			}
+		}
+	}
 
-    handleInput(messageObject) {
-        if (!messageObject.content.startsWith(this.prefix) || messageObject.author.bot) {
-            return;
-        }
+	handleInput(messageObject) {
+		if (!messageObject.content.startsWith(this.prefix) || messageObject.author.bot) {
+			return;
+		}
 
-        const args = messageObject.content.slice(this.prefix.length).split(this.delimiter + '+');
-        const commandName = args.shift().toLowerCase();
+		const args = messageObject.content.slice(this.prefix.length).split(this.delimiter + '+');
+		const commandName = args.shift().toLowerCase();
 
-        if(!this.commands.has(commandName)){
-            return;
-        }
+		if(!this.commands.has(commandName)){
+			return;
+		}
 
-        try {
-            this.commands.get(commandName).method(messageObject, args, this);
-        } catch (err) {
-            Logger.logMessage('ERROR', err);
-            this.sendOutput(messageObject.channel, "I had trouble executing that command. Please refer to your admin for more information.");
-        }
+		try {
+			this.commands.get(commandName).method(messageObject, args, this);
+		} catch (err) {
+			Logger.logMessage('ERROR', err);
+			this.sendOutput(messageObject.channel, "I had trouble executing that command. Please refer to your admin for more information.");
+		}
 
-        const messageLogString = `${messageObject.author.username}[${parseInt(messageObject.author.id, 10).toString(36)}] (${messageObject.embeds.length} embeded data): ${messageObject.cleanContent}`;
-        Logger.logMessage('DEBUG', messageLogString);
-    }
+		const messageLogString = `${messageObject.author.username}[${parseInt(messageObject.author.id, 10).toString(36)}] (${messageObject.embeds.length} embeded data): ${messageObject.cleanContent}`;
+		Logger.logMessage('DEBUG', messageLogString);
+	}
 
-    sendOutput(channel, messageString, isCode=false, title='') {
-        let sendFunction;
-        if(!isCode){ 
-            sendFunction = () => channel.send(messageString);
-        } else {
-            const embed = new Discord.RichEmbed()
-            .setTitle(title)
-            .setDescription(messageString)
-            .setColor(16763981)
-            .setFooter(`Use ${this.prefix}help to get help`);
-            sendFunction = () => channel.send(embed);
-        } 
-        sendFunction(messageString).then((messageSent) => {
-            const messageLogString = `${messageSent.author.username}[${parseInt(messageSent.author.id, 10).toString(36)}] (${messageSent.embeds.length} embeded data): ${messageSent.cleanContent}`;
-            Logger.logMessage('DEBUG', messageLogString);
-        }).catch((err) => {
-            Logger.logMessage('ERROR', err);
-        });
-    }
+	sendOutput(channel, messageString, isCode=false, title='') {
+		let sendFunction;
+		if(!isCode){ 
+			sendFunction = () => channel.send(messageString);
+		} else {
+			const embed = new Discord.RichEmbed()
+			.setTitle(title)
+			.setDescription(messageString)
+			.setColor(16763981)
+			.setFooter(`Use ${this.prefix}help to get help`);
+			sendFunction = () => channel.send(embed);
+		} 
+		sendFunction(messageString).then((messageSent) => {
+			const messageLogString = `${messageSent.author.username}[${parseInt(messageSent.author.id, 10).toString(36)}] (${messageSent.embeds.length} embeded data): ${messageSent.cleanContent}`;
+			Logger.logMessage('DEBUG', messageLogString);
+		}).catch((err) => {
+			Logger.logMessage('ERROR', err);
+		});
+	}
 
-    initMessage() {
-        Logger.logMessage('INFO','Ready, ID: ' + this.id);
-    }
+	initMessage() {
+		Logger.logMessage('INFO','Ready, ID: ' + this.id);
+	}
 }
 
 module.exports = Bot;
--- src/logger.js
+++ src/logger.js
@@ -1,80 +1,80 @@
 const config = require('../docs/deploy/config.json');
 const fs = require('fs');
 const logPath = '../logs/';
-const logFileName = 'log' + new Date().getTime() + '.log'
+const logFileName = 'log' + new Date().getTime() + '.log';
 
 class Logger {
 
-    static logMessage(logLevel, message) {
-        //onetime init for logstream
-        if(typeof Logger.logStream === 'undefined') {
-            if(!fs.existsSync(logPath)) {
-                fs.mkdirSync(logPath, {recursive: true});
-            }
-            Logger.logStream = fs.createWriteStream(logPath + logFileName, {flag: 'a'});
-            //remove extra/unneeded log files
-            Logger.logStream.on('open', () => { //Wait until the file is actually opened to count how many files are in directory to avoid race conditions
-                fs.readdir(logPath, (err, files) => {
-                    let dataFiles = files.map((file) => {
-                        const stat = fs.statSync(logPath + file);
-                        return {
-                            name: file,
-                            time: stat.birthtimeMs
-                        };
-                    });
+	static logMessage(logLevel, message) {
+		//onetime init for logstream
+		if(typeof Logger.logStream === 'undefined') {
+			if(!fs.existsSync(logPath)) {
+				fs.mkdirSync(logPath, {recursive: true});
+			}
+			Logger.logStream = fs.createWriteStream(logPath + logFileName, {flag: 'a'});
+			//remove extra/unneeded log files
+			Logger.logStream.on('open', () => { //Wait until the file is actually opened to count how many files are in directory to avoid race conditions
+				fs.readdir(logPath, (err, files) => {
+					let dataFiles = files.map((file) => {
+						const stat = fs.statSync(logPath + file);
+						return {
+							name: file,
+							time: stat.birthtimeMs
+						};
+					});
 
-                    dataFiles.filter(dataFile => dataFile.name.endsWith('.log'));
-                    
-                    dataFiles.sort((file1, file2) => {
-                        return file2.time - file1.time;
-                    });
-                    
-                    while(dataFiles.length > config.maxLogFiles) {
-                        const toRemove = dataFiles.pop();
-                        fs.unlink(logPath + toRemove.name, (err) => {
-                            if(err) {
-                                console.log('Error when attempting to remove Log File ' + toRemove.name + ': ' + err);
-                            }
-                        });
-                    }
-                })
-            });
-        }
-        if(this.rankLogLevel(logLevel) >= this.rankLogLevel(config.logLevel)) {
-            const logString = "| [" + new Date() + "] " + logLevel.toUpperCase() + " |: " + message;
-            Logger.logStream.write(logString + "\n");
-            console.log(logString);
-        }
-    }
+					dataFiles.filter(dataFile => dataFile.name.endsWith('.log'));
+					
+					dataFiles.sort((file1, file2) => {
+						return file2.time - file1.time;
+					});
+					
+					while(dataFiles.length > config.maxLogFiles) {
+						const toRemove = dataFiles.pop();
+						fs.unlink(logPath + toRemove.name, (err) => {
+							if(err) {
+								console.log('Error when attempting to remove Log File ' + toRemove.name + ': ' + err);
+							}
+						});
+					}
+				})
+			});
+		}
+		if(this.rankLogLevel(logLevel) >= this.rankLogLevel(config.logLevel)) {
+			const logString = "| [" + new Date() + "] " + logLevel.toUpperCase() + " |: " + message;
+			Logger.logStream.write(logString + "\n");
+			console.log(logString);
+		}
+	}
 
-    static rankLogLevel(logLevel) {
-        logLevel = logLevel.toUpperCase();
-        switch(logLevel) {
-            case 'ALL':
-                return 0;
-            case 'DEBUG':
-                return 2;
-            case 'INFO':
-                return 3;
-            case 'WARN':
-                return 4;
-            case 'ERROR':
-                return 5;
-            case 'NONE':
-                return 6;
-            default:
-                return 3;
-        }
-    }
+	static rankLogLevel(logLevel) {
+		logLevel = logLevel.toUpperCase();
+		switch(logLevel) {
+			case 'ALL':
+				return 0;
+			case 'DEBUG':
+				return 2;
+			case 'INFO':
+				return 3;
+			case 'WARN':
+				return 4;
+			case 'ERROR':
+				return 5;
+			case 'NONE':
+				return 6;
+			default:
+				return 3;
+		}
+	}
 
-    static closeLogStream() {
-        if (typeof Logger.logStream != 'undefined') {
-            this.logMessage('INFO', 'Exiting... Ending Write Stream to Log');
-            Logger.logStream.end();
-        }
-    }
+	static closeLogStream() {
+		if (typeof Logger.logStream != 'undefined') {
+			this.logMessage('INFO', 'Exiting... Ending Write Stream to Log');
+			Logger.logStream.end();
+		}
+	}
 
-        
+		
 }
 
 module.exports = Logger;
